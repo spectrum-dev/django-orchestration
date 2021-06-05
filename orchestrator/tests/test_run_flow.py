4e5a1628aca96b2dd9851dde19e6ce19b2cc7ad8
Rahul V Brahmal <rahul.brahmal@me.com>
2021-06-05 14:20:38 +0100

    (fix): Linting.

--- orchestrator/tests/test_run_flow.py
+++ orchestrator/tests/test_run_flow.py
@@ -1,4 +1,3 @@
-import json
 from django.test import TestCase
 
 from orchestrator.services.flow.spectrum_flow_v2 import SpectrumFlow
@@ -9,7 +8,7 @@ class RunFlowTest(TestCase):
     def test_full_single_flow_valid(self):
         flow = SpectrumFlow(SINGLE_FULL_FLOW_VALID["nodeList"], SINGLE_FULL_FLOW_VALID["edgeList"])
 
-        response = flow.run(mode="RUN")
+        flow.run(mode="RUN")
 
         # self.assertDictEqual(response, TEST_FULL_SINGLE_FLOW_VALID)
         
