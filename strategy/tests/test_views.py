f927db996e20d64eaa72672189c21c8dcab19cbd
Rahul V Brahmal <rahul.brahmal@me.com>
2021-06-26 07:41:06 +0100

    (test): Fixed failing test due to UUID generation.

--- strategy/tests/test_views.py
+++ strategy/tests/test_views.py
@@ -29,7 +29,7 @@ class StrategyIdViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertDictEqual(
             response.json(),
-            {'strategy_id': '00000000-0000-0000-0000-000000000001'}
+            {'strategy_id': '00000000-0000-0000-0000-000000000002'}
         )
     
     @patch('uuid.uuid4', fixed_mock_uuid)
