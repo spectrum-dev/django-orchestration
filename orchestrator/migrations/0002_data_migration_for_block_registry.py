0cab13a7808ddced687c8b4a8da22b6761b36fa8
Rahul V Brahmal <rahul.brahmal@me.com>
2021-08-07 14:10:27 +0100

    (feature): Added metadata for data migration block.

--- orchestrator/migrations/0002_data_migration_for_block_registry.py
+++ orchestrator/migrations/0002_data_migration_for_block_registry.py
@@ -167,6 +167,42 @@ def seed_blocks_into_registry(apps, schema_editor):
     ).save()
 
     BlockRegistry(
+        block_type="SIGNAL_BLOCK",
+        block_id=4,
+        block_name="Crossover",
+        inputs=[
+            {
+                "fieldData": {"base": "/eventType", "method": "GET"},
+                "fieldName": "Event Type",
+                "fieldType": "dropdown",
+                "fieldVariableName": "event_type",
+            },
+            {
+                "fieldName": "Event Value",
+                "fieldVariableName": "event_value",
+                "fieldType": "input",
+            },
+            {
+                "fieldData": {"base": "/eventAction", "method": "GET"},
+                "fieldName": "Event Action",
+                "fieldType": "dropdown",
+                "fieldVariableName": "event_action",
+            },
+        ],
+        validations={
+            "input": {
+                "required": [{"blockType": "COMPUTATIONAL_BLOCK", "number": 1}],
+                "allowed_blocks": [
+                    {"blockId": "1", "blockType": "COMPUTATIONAL_BLOCK"},
+                    {"blockId": "1", "blockType": "DATA_BLOCK"},
+                    {"blockId": "2", "blockType": "DATA_BLOCK"},
+                ],
+            },
+            "output": [{"blockType": "SIGNAL_BLOCK", "number": 1}],
+        },
+    ).save()
+
+    BlockRegistry(
         block_type="STRATEGY_BLOCK",
         block_id=1,
         block_name="Backtest",
