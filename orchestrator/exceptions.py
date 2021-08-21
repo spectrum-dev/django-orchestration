4f946d2796025554b5d74cfdeb4c36ccb0cb3411
Rahul V Brahmal <rahul.brahmal@me.com>
2021-08-21 14:29:12 +0100

    (feature): Implemented run as a celery task.

---
+++ orchestrator/exceptions.py
@@ -0,0 +1,9 @@
+class StrategyNotValidException(Exception):
+    """
+        Strategy is invalid
+    """
+
+class StrategyDoesNotExistException(Exception):
+    """
+        Strategy does not exist
+    """
