108bd5e7663d68ac82512100416c896ee572baf4
Rahul V Brahmal <rahul.brahmal@me.com>
2021-10-04 12:21:45 +0100

    (fix): Implemented collectstatic.

--- orchestration/settings.py
+++ orchestration/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import logging
 import sentry_sdk
 
-from os import environ
+from os import environ, path
 from dotenv import load_dotenv
 
 from corsheaders.defaults import default_headers
@@ -179,6 +179,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
