ab2dc197f2e9b6a7d900cc7c71bd2ab5b21a22e2
Rahul V Brahmal <rahul.brahmal@me.com>
2021-10-23 11:45:17 +0100

    (feature): Refactored the email whitelist tests.

---
+++ authentication/tests/test_email_whitelist.py
@@ -0,0 +1,47 @@
+from authentication.factories import AccountWhitelistFactory, set_up_authentication
+from orchestration.test_utils import GraphQLTestCase
+
+
+class ValidateEmailWhitelistTest(GraphQLTestCase):
+    def setUp(self):
+        self.AUTHENTICATION_QUERY = """
+            mutation validateAccountWhitelist($email: String!) {
+                validateAccountWhitelist(email: $email) {
+                    status
+                }
+            }
+        """
+        self.auth = set_up_authentication()
+
+    def test_email_exists_and_active(self):
+        AccountWhitelistFactory(
+            email="valid@testcustomer.com", active=True
+        )
+        response, content = self.query(
+            self.AUTHENTICATION_QUERY,
+            headers={"HTTP_AUTHORIZATION": f"Bearer {self.auth['token']}"},
+            variables={'email': 'valid@testcustomer.com'},
+        )
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual(content["data"], {'validateAccountWhitelist': {'status': True}})
+
+    def test_email_exists_not_active(self):
+        AccountWhitelistFactory(
+            email="valid@testcustomer.com", active=False
+        )
+        response, content = self.query(
+            self.AUTHENTICATION_QUERY,
+            headers={"HTTP_AUTHORIZATION": f"Bearer {self.auth['token']}"},
+            variables={'email': 'valid@testcustomer.com'},
+        )
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual(content["data"], {'validateAccountWhitelist': {'status': False}})
+    
+    def test_email_dne(self):
+        response, content = self.query(
+            self.AUTHENTICATION_QUERY,
+            headers={"HTTP_AUTHORIZATION": f"Bearer {self.auth['token']}"},
+            variables={'email': 'valid@testcustomer.com'},
+        )
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual(content["data"], {'validateAccountWhitelist': {'status': False}})
