c154d217cc17e13a2fc1022a014fe583e1704acb
Rahul V Brahmal <rahul.brahmal@me.com>
2021-11-08 12:05:42 +0000

    (feature): Migrated tests over to GQL and deleted deprecated folders.

--- strategy/resolvers.py
+++ strategy/resolvers.py
@@ -27,51 +27,55 @@ def get_user_strategy(*_, strategy_id):
 
 
 def get_strategy(_, info, strategyId, commitId=None):
-    user_strategy = UserStrategy.objects.filter(strategy=strategyId)
-    strategy_sharing = StrategySharing.objects.filter(
-        strategy__strategy=strategyId, user=info.context["user"]
-    )
+    try:
+        user_strategy = UserStrategy.objects.filter(
+            strategy=strategyId, user=info.context["user"]
+        )
+        strategy_sharing = StrategySharing.objects.filter(
+            strategy__strategy=strategyId, user=info.context["user"]
+        )
 
-    if user_strategy.exists() or strategy_sharing.exists():
-        if user_strategy.exists():
-            user_strategy = user_strategy[0]
+        if user_strategy.exists() or strategy_sharing.exists():
+            if user_strategy.exists():
+                user_strategy = user_strategy[0]
 
-        if strategy_sharing.exists():
-            strategy_sharing = strategy_sharing[0]
+            if strategy_sharing.exists():
+                strategy_sharing = strategy_sharing[0]
 
-        if commitId:
-            strategy = Strategy.objects.get(
-                strategy=user_strategy[0],
-                commit=commitId,
-            )
-
-        else:
-            strategy = (
-                Strategy.objects.filter(
-                    strategy__strategy=strategyId,
+            strategy = None
+            if commitId:
+                strategy = Strategy.objects.get(
+                    strategy=user_strategy,
+                    commit=commitId,
                 )
-                .order_by("-updated_at")
-                .first()
-            )
 
-        response = {
-            "strategy": {
-                "strategyId": strategyId,
-                "strategyName": user_strategy.strategy_name,
-                "createdAt": user_strategy.created_at,
-                "updatedAt": user_strategy.updated_at,
-            },
-            "commitId": str(strategy.commit),
-            "flowMetadata": strategy.flow_metadata,
-            "input": strategy.input,
-            "output": strategy.output,
-            "createdAt": strategy.created_at,
-            "updatedAt": strategy.updated_at,
-        }
-        print(response)
-        return response
-    else:
-        raise Exception("You are not authorized to view this strategy")
+            else:
+                strategy = (
+                    Strategy.objects.filter(
+                        strategy__strategy=strategyId,
+                    )
+                    .order_by("-updated_at")
+                    .first()
+                )
+
+            return {
+                "strategy": {
+                    "strategy_id": strategyId,
+                    "strategy_name": user_strategy.strategy_name,
+                    "created_at": user_strategy.created_at,
+                    "updated_at": user_strategy.updated_at,
+                },
+                "commit_id": str(strategy.commit),
+                "flow_metadata": strategy.flow_metadata,
+                "input": strategy.input,
+                "output": strategy.output,
+                "created_at": strategy.created_at,
+                "updated_at": strategy.updated_at,
+            }
+        else:
+            raise Exception("You are not authorized to view this strategy")
+    except Strategy.DoesNotExist:
+        raise Exception("The strategy and commit pair does not exist")
 
 
 @convert_kwargs_to_snake_case
--- strategy/tests/test_get_strategy.py
+++ strategy/tests/test_get_strategy.py
@@ -1,6 +1,10 @@
 from authentication.factories import set_up_authentication
 from orchestration.test_utils import GraphQLTestCase
-from strategy.factories import StrategyFactory, UserStrategyFactory
+from strategy.factories import (
+    StrategyFactory,
+    StrategySharingFactory,
+    UserStrategyFactory,
+)
 
 
 class GetStrategyTest(GraphQLTestCase):
@@ -9,19 +13,156 @@ class GetStrategyTest(GraphQLTestCase):
             query STRATEGY($strategyId: ID!) {
                 strategy(strategyId: $strategyId) {
                     strategy {
+                        strategyId
                         strategyName
                     }
+                    commitId
+                    flowMetadata
+                    input
+                    output
+                }
+            }
+        """
+
+        self.QUERY_WITH_COMMIT = """
+            query STRATEGY($strategyId: ID!, $commitId: ID) {
+                strategy(strategyId: $strategyId, commitId: $commitId) {
+                    strategy {
+                        strategyId
+                        strategyName
+                    }
+                    commitId
+                    flowMetadata
+                    input
+                    output
                 }
             }
         """
 
         self.auth = set_up_authentication()
 
-    def test_ok(self):
+    def test_no_commit_id_returns_latest_strategy(self):
+        strategy_id = "5f4a0050-6766-40e1-946c-ddbd5533a3d1"
+        commit_id_one = "28061176-a818-4525-a238-c9a73c6418f1"
+        commit_id_two = "28061176-a818-4525-a238-c9a73c6418f2"
+        user_strategy = UserStrategyFactory(
+            user=self.auth["user"], strategy=strategy_id, strategy_name="Test Name"
+        )
+
+        StrategyFactory(
+            strategy=user_strategy,
+            commit=commit_id_one,
+            flow_metadata={},
+            input={},
+            output={},
+        )
+
+        StrategyFactory(
+            strategy=user_strategy,
+            commit=commit_id_two,
+            flow_metadata={},
+            input={"test": "test"},
+            output={},
+        )
+
+        response, content = self.query(
+            self.QUERY,
+            headers={"HTTP_AUTHORIZATION": f"Bearer {self.auth['token']}"},
+            variables={"strategyId": strategy_id},
+        )
+
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual(
+            content["data"],
+            {
+                "strategy": {
+                    "strategy": {
+                        "strategyId": "5f4a0050-6766-40e1-946c-ddbd5533a3d1",
+                        "strategyName": "Test Name",
+                    },
+                    "commitId": "28061176-a818-4525-a238-c9a73c6418f2",
+                    "flowMetadata": {},
+                    "input": {"test": "test"},
+                    "output": {},
+                }
+            },
+        )
+
+    def test_commit_id_returns_specific_strategy(self):
+        strategy_id = "5f4a0050-6766-40e1-946c-ddbd5533a3d1"
+        commit_id_one = "28061176-a818-4525-a238-c9a73c6418f1"
+        commit_id_two = "28061176-a818-4525-a238-c9a73c6418f2"
+        user_strategy = UserStrategyFactory(
+            user=self.auth["user"], strategy=strategy_id, strategy_name="Test Name"
+        )
+
+        StrategyFactory(
+            strategy=user_strategy,
+            commit=commit_id_one,
+            flow_metadata={},
+            input={},
+            output={},
+        )
+
+        StrategyFactory(
+            strategy=user_strategy,
+            commit=commit_id_two,
+            flow_metadata={},
+            input={"test": "test"},
+            output={},
+        )
+
+        response, content = self.query(
+            self.QUERY_WITH_COMMIT,
+            headers={"HTTP_AUTHORIZATION": f"Bearer {self.auth['token']}"},
+            variables={"strategyId": strategy_id, "commitId": commit_id_one},
+        )
+
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual(
+            content["data"],
+            {
+                "strategy": {
+                    "strategy": {
+                        "strategyId": "5f4a0050-6766-40e1-946c-ddbd5533a3d1",
+                        "strategyName": "Test Name",
+                    },
+                    "commitId": "28061176-a818-4525-a238-c9a73c6418f1",
+                    "flowMetadata": {},
+                    "input": {},
+                    "output": {},
+                }
+            },
+        )
+
+    def test_strategy_id_dne(self):
+        strategy_id = "5f4a0050-6766-40e1-946c-ddbd5533a3d1"
+
+        response, content = self.query(
+            self.QUERY,
+            headers={"HTTP_AUTHORIZATION": f"Bearer {self.auth['token']}"},
+            variables={"strategyId": strategy_id},
+        )
+
+        self.assertResponseHasErrors(response)
+        self.assertEqual(
+            content["errors"],
+            [
+                {
+                    "locations": [{"column": 17, "line": 3}],
+                    "message": "You are not authorized to view this strategy",
+                    "path": ["strategy"],
+                }
+            ],
+        )
+
+    def test_strategy_id_does_not_belong_to_user_and_is_not_shared(self):
+        other_user = set_up_authentication()
         strategy_id = "5f4a0050-6766-40e1-946c-ddbd5533a3d1"
         commit_id = "28061176-a818-4525-a238-c9a73c6418f1"
+
         user_strategy = UserStrategyFactory(
-            user=self.auth["user"], strategy=strategy_id
+            user=other_user["user"], strategy=strategy_id
         )
 
         StrategyFactory(
@@ -39,4 +180,80 @@ class GetStrategyTest(GraphQLTestCase):
         )
 
         self.assertResponseHasErrors(response)
-        self.assertDictEqual(content["data"], {})
+        self.assertEqual(
+            content["errors"],
+            [
+                {
+                    "locations": [{"column": 17, "line": 3}],
+                    "message": "You are not authorized to view this strategy",
+                    "path": ["strategy"],
+                }
+            ],
+        )
+
+    def test_shared_user_can_view_strategy(self):
+        shared_auth = set_up_authentication()
+        strategy_id = "5f4a0050-6766-40e1-946c-ddbd5533a3d1"
+        commit_id = "28061176-a818-4525-a238-c9a73c6418f1"
+
+        user_strategy = UserStrategyFactory(
+            user=self.auth["user"], strategy=strategy_id
+        )
+        StrategyFactory(
+            strategy=user_strategy,
+            commit=commit_id,
+            flow_metadata={},
+            input={},
+            output={},
+        )
+
+        StrategySharingFactory(
+            strategy=user_strategy, user=shared_auth["user"], permissions=2
+        )
+
+        response, content = self.query(
+            self.QUERY,
+            headers={"HTTP_AUTHORIZATION": f"Bearer {self.auth['token']}"},
+            variables={"strategyId": strategy_id},
+        )
+
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual(
+            content["data"],
+            {
+                "strategy": {
+                    "strategy": {
+                        "strategyId": "5f4a0050-6766-40e1-946c-ddbd5533a3d1",
+                        "strategyName": "",
+                    },
+                    "commitId": "28061176-a818-4525-a238-c9a73c6418f1",
+                    "flowMetadata": {},
+                    "input": {},
+                    "output": {},
+                }
+            },
+        )
+
+    def test_commit_id_dne(self):
+        strategy_id = "5f4a0050-6766-40e1-946c-ddbd5533a3d1"
+        commit_id = "26fed45a-20f3-47b0-819c-c17bfe22774a"
+
+        UserStrategyFactory(user=self.auth["user"], strategy=strategy_id)
+
+        response, content = self.query(
+            self.QUERY_WITH_COMMIT,
+            headers={"HTTP_AUTHORIZATION": f"Bearer {self.auth['token']}"},
+            variables={"strategyId": strategy_id, "commitId": commit_id},
+        )
+
+        self.assertResponseHasErrors(response)
+        self.assertEqual(
+            content["errors"],
+            [
+                {
+                    "locations": [{"column": 17, "line": 3}],
+                    "message": "The strategy and commit pair does not exist",
+                    "path": ["strategy"],
+                }
+            ],
+        )
