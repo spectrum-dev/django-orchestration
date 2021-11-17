0ce0fb0d097d5fd51d1994ad2c974ecfd0ad0d13
Rahul Brahmal <rahul.brahmal@me.com>
2021-11-17 20:32:50 +0000

    (feature): Added model and migration for BasicAuthToken support.

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -5,3 +6,8 @@ from django.db import models
 class AccountWhitelist(models.Model):
     email = models.EmailField(unique=True)
     active = models.BooleanField()
+
+
+class BasicAuthToken(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    token = models.TextField()
