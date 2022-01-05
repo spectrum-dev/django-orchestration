6293e219069c4e9be258d8833af102d2856647df
Rahul Brahmal <rahul.brahmal@me.com>
2022-01-05 11:57:30 +0800

    (remove): Removed the mock classes.

--- orchestrator/tests/data/fixture_mock.py
+++ orchestrator/tests/data/fixture_mock.py
@@ -3,37 +3,3 @@
 class GenericCeleryMockClass:
     def get(self):
         return "mock-result-value-here"
-
-
-MULTIPLE_STRATEGY_BLOCK_MOCK_RESPONSE = [
-    (
-        "DATA_BLOCK-1-1",
-        GenericCeleryMockClass(),
-        None,
-    ),
-    (
-        "COMPUTATIONAL_BLOCK-1-3",
-        GenericCeleryMockClass(),
-        None,
-    ),
-    (
-        "COMPUTATIONAL_BLOCK-1-2",
-        GenericCeleryMockClass(),
-        None,
-    ),
-    (
-        "SIGNAL_BLOCK-1-4",
-        GenericCeleryMockClass(),
-        None,
-    ),
-    (
-        "STRATEGY_BLOCK-1-5",
-        GenericCeleryMockClass(),
-        None,
-    ),
-    (
-        "STRATEGY_BLOCK-1-6",
-        GenericCeleryMockClass(),
-        None,
-    ),
-]
