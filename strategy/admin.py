f1cc31e8481c2ad32a299ea2caa632a60d09221e
Rahul Brahmal <rahul.brahmal@me.com>
2022-03-06 12:07:07 +0000

    (fix): Added admin.py.

--- strategy/admin.py
+++ strategy/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from strategy.models import Strategy, StrategySharing, UserStrategy
 
-
 admin.site.register(Strategy)
 admin.site.register(UserStrategy)
 admin.site.register(StrategySharing)
